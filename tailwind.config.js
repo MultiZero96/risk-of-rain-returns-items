e1920edf54e6cb4c19582472f93b3e547f4f9dbd
vt <vuk.tanaskovic@studenti.polito.it>
2023-11-26 12:57:45 +0100

    node modules adn index

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'risk-of-rain-logo': "url('https://riskofrainreturns.wiki.gg/images/e/e6/Site-logo.png?aeb81')",
+        'risk-of-rain': "url('/src/riskofrain-1700998525367-1992.jpg')",
+      },
+      fontFamily: {
+        minecraft: ["MINECRAFT", "sans-serif"],
+      },
+    },
+    plugins: [],
+  }
+}
